aa4c1a56cca36c81295d5a76d540fd57ee6fd2be
Yaba Ernesto <yabaernesto@gmail.com>
2025-04-19 00:03:51 +0100

    feat: Icones do header adicionados

--- services/header.php
+++ services/header.php
@@ -5,6 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="../assets/css/header.css" />
+  <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    />
 </head>
 <body>
   
@@ -13,13 +17,13 @@
 <div class="header-navigation">
   <nav class="menu-principal">
     <ul class="list">
-      <li><a href="../pages/produtos.php">Produtos</a></li>
-      <li><a href="../pages/categorias.php">Categorias</a></li>
-      <li><a href="../pages/fornecedores.php">Fornecedores</a></li>
-      <li><a href="../pages/armazens.php">Armazens</a></li>
-      <li><a href="../pages/movimentacoes.php">Movimentação</a></li>
-      <li><a href="../pages/relatorios.php">Relatórios</a></li>
-      <li><a href="../pages/notificacoes.php">Notificações</a></li>
+      <li><a href="../pages/produtos.php"><i class="fas fa-box" style="color: #3498db;"></i> Produtos</a></li>
+      <li><a href="../pages/categorias.php"><i class="fas fa-tags" style="color: #9b59b6;"></i> Categorias</a></li>
+      <li><a href="../pages/fornecedores.php"><i class="fas fa-truck" style="color: #e67e22;"></i> Fornecedores</a></li>
+      <li><a href="../pages/armazens.php"><i class="fas fa-warehouse" style="color: #27ae60;"></i> Armazéns</a></li>
+      <li><a href="../pages/movimentacoes.php"><i class="fas fa-exchange-alt" style="color: #1abc9c;"></i> Movimentação</a></li>
+      <li><a href="../pages/relatorios.php"><i class="fas fa-chart-line" style="color: #e74c3c;"></i> Relatórios</a></li>
+      <li><a href="../pages/notificacoes.php"><i class="fas fa-bell" style="color: #f1c40f;"></i> Notificações</a></li>
     </ul>
   </nav>
 </div>
