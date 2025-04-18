30e3793e8d25dc325249819e49acd6cb9a1175f7
Yaba Ernesto <yabaernesto@gmail.com>
2025-04-18 22:00:36 +0100

    chore: organização das pastas

--- services/dashboard.php
+++ services/dashboard.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard - Sistema de Gestão de Inventário</title>
-    <link rel="stylesheet" href="assets/css/estilo.css" />
-    <link rel="stylesheet" href="assets/css/dashboard.css" />
+    <link rel="stylesheet" href="../assets/css/estilo.css" />
+    <link rel="stylesheet" href="../assets/css/dashboard.css" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
